0afd3c78ab427fc2c86eb0962450b2a3b86b527b
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2019-02-03 17:36:23 +0100

    Fix OutOfRangeException

--- src/main/scala/jb/MultiRunner.scala
+++ src/main/scala/jb/MultiRunner.scala
@@ -9,21 +9,23 @@ object MultiRunner {
 
   def main(args: Array[String]): Unit = {
     SparkEmbedded.setLogWarn()
-    val reps = 10
+    val reps = 1
     val filenames = Array("bi", "bu", "c", "d", "h", "i", "m", "p", "se", "so", "sp", "t", "wd", "wi")
     val nClassif = 3
-    val nFeatures = 3
+    val nFeatures = 4
     val divisions = Array(20, 40, 60)
 
     val runner = new Runner(nClassif, nFeatures, divisions)
     val finalScores = runForFiles(reps, runner)(filenames)
 
     writeScores(finalScores)
+    println(finalScores.map(sc => if (sc.last > sc.head) 1 else 0).sum.toDouble / finalScores.length)
   }
 
   private def runForFiles(reps: Int, runner: Runner)(filenames: Array[String]) = {
     val finalScores = new Array[Array[Double]](filenames.length)
     for (index <- filenames.indices) {
+      println(s"File: ${filenames(index)}")
       finalScores(index) = runReps(reps, runner, filenames(index))
     }
     finalScores

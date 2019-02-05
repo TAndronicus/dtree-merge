86d1a84bb31cf1864a6fccba4c109b0b70947bc0
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2019-02-05 19:44:28 +0100

    Reformat multirunner

--- src/main/scala/jb/MultiRunner.scala
+++ src/main/scala/jb/MultiRunner.scala
@@ -2,24 +2,19 @@ package jb
 
 import java.io.{File, PrintWriter}
 
-import jb.server.SparkEmbedded
 import jb.util.Const.FILENAME_PREFIX
 
 object MultiRunner {
 
-  def main(args: Array[String]): Unit = {
-    SparkEmbedded.setLogWarn()
+  def run(nClassif: Int, nFeatures: Int, divisions: Array[Int]): Double = {
     val reps = 1
     val filenames = Array("bi", "bu", "c", "d", "h", "i", "m", "p", "se", "so", "sp", "t", "wd", "wi")
-    val nClassif = 3
-    val nFeatures = 4
-    val divisions = Array(20, 40, 60)
 
     val runner = new Runner(nClassif, nFeatures, divisions)
     val finalScores = runForFiles(reps, runner)(filenames)
 
-    writeScores(finalScores)
-    println(finalScores.map(sc => if (sc.last > sc.head) 1 else 0).sum.toDouble / finalScores.length)
+    //    writeScores(finalScores)
+    finalScores.map(sc => if (sc.last > sc.head) 1 else 0).sum.toDouble / finalScores.length
   }
 
   private def runForFiles(reps: Int, runner: Runner)(filenames: Array[String]) = {
--- src/main/scala/jb/Playground.scala
+++ src/main/scala/jb/Playground.scala
@@ -1,28 +1,36 @@
 package jb
 
+import jb.server.SparkEmbedded
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
 
 case class FooResponse(foos: Seq[Foo])
+
 case class Foo(id: String, typek: String, color: String)
 
 object Playground {
 
-      implicit val ec: ExecutionContext = ExecutionContext.global
+  implicit val ec: ExecutionContext = ExecutionContext.global
+
   def main(args: Array[String]): Unit = {
-//        val conf = new SparkConf().setAppName("dtree-merge").setMaster("local")
-//        val ss = SparkSession.builder.config(conf).getOrCreate
-//        val s1 = SparkEmbedded.ss.read.option("inferSchema", "true").format("csv").load("A/bi").rdd
-//    val s1 = ss.sparkContext.textFile("A/bi")
-//    println(s1.map(_.getBytes("UTF-8").length.toLong).reduce(_+_))
-//        val s2 = SparkEmbedded.ss.read.option("inferSchema", "true").format("csv").load("A/a.csv")
+    val conf = new SparkConf().setAppName("dtree-merge").setMaster("local")
+    val ss = SparkSession.builder.config(conf).getOrCreate
+    //    val s1 = ss.sparkContext.textFile("A/bi")
+    //        val s2 = SparkEmbedded.ss.read.option("inferSchema", "true").format("csv").load("A/a.csv")
     //    s1.unionAll(s2).except(s1.intersect(s2)).show()
-    //    print("### Second ###")
     //    s1.except(s2).union(s2.except(s1)).show()
+
+    val df = SparkEmbedded.ss.createDataFrame(Seq(
+      (1, 2, 5),
+      (2, 4, 7),
+      (3, 6, 9)
+    )).toDF("label", "min", "max")
+
+    print("heh")
+
 
 
   }

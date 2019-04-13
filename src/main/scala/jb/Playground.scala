ef2423e4b6334721908beabaea4d2a9cb63d34bb
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2019-04-13 08:42:46 +0200

    GPG test

--- src/main/scala/jb/Playground.scala
+++ src/main/scala/jb/Playground.scala
@@ -13,28 +13,7 @@ case class Foo(id: String, typek: String, color: String)
 
 object Playground {
 
-  implicit val ec: ExecutionContext = ExecutionContext.global
-
   def main(args: Array[String]): Unit = {
-    isList(List(1, 2, 3))
-  }
-
-  def sth[A, B, C]: (A => B) => C = ???
-
-  def parseDouble(value: Any): Double = {
-    value match {
-      case int: Int =>
-        int.toDouble
-      case double: Double =>
-        double
-    }
-  }
-
-  def isList(a: Any): Unit = {
-    a match {
-      case a: List[AnyVal] => println("Heh")
-      case _ => println("Jednak nie")
-    }
   }
 
 }

70e6bd61e3b83328947e066db178096092b0b88f
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2019-07-31 20:55:09 +0200

    Change config

--- src/main/scala/jb/conf/Config.scala
+++ src/main/scala/jb/conf/Config.scala
@@ -7,7 +7,7 @@ object Config {
   val impurity = "gini"
 
   /** Result catcher */
-  val treshold: Double = .2
+  val treshold: Double = .4
   val batch: Int = 10
   val minIter: Int = 50
   val maxIter: Int = Int.MaxValue

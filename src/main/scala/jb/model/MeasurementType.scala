246e31b7dfc82917dcdead23bebc17eda3310a28
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2020-08-01 03:51:29 +0200

    Fix model

--- src/main/scala/jb/model/MeasurementType.scala
+++
@@ -1,7 +0,0 @@
-package jb.model
-
-sealed class MeasurementType(val priority: Int)
-
-case class Acc() extends MeasurementType(0)
-
-case class Mcc() extends MeasurementType(1)
--- src/main/scala/jb/model/Measurements.scala
+++ src/main/scala/jb/model/Measurements.scala
@@ -1,9 +1,21 @@
 package jb.model
 
 case class Measurements(
-                         measurementType: MeasurementType,
-                         mv: Double,
-                         rf: Double,
-                         wMvs: Array[Double],
-                         i: Array[Double]
-                       )
+                         acc: Double,
+                         precissionMi: Double,
+                         recallMi: Double,
+                         fScoreMi: Double,
+                         precissionM: Double,
+                         recallM: Double,
+                         fScoreM: Double
+                       ) {
+  def toArray: Array[Double] = Array(
+    acc: Double,
+    precissionMi: Double,
+    recallMi: Double,
+    fScoreMi: Double,
+    precissionM: Double,
+    recallM: Double,
+    fScoreM: Double
+  )
+}

ba1435443c8439bad9da8329fe5a2b56b664e1c3
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2020-08-01 10:52:12 +0200

    Refactor to use Measurements

--- src/main/scala/jb/tester/FullTester.scala
+++ src/main/scala/jb/tester/FullTester.scala
@@ -1,7 +1,7 @@
 package jb.tester
 
 import jb.conf.Config
-import jb.model.Cube
+import jb.model.{Cube, Measurements}
 import jb.util.Const.{FEATURES, LABEL, PREDICTION}
 import org.apache.spark.ml.classification.RandomForestClassifier
 import org.apache.spark.ml.linalg.{DenseVector, SparseVector}
@@ -10,7 +10,7 @@ import org.apache.spark.sql.DataFrame
 object FullTester {
 
 
-  def testMv(testSubset: DataFrame, nClassif: Int): (Double, Double) = {
+  def testMv(testSubset: DataFrame, nClassif: Int): Measurements = {
     val cols = for (i <- 0.until(nClassif)) yield PREDICTION + "_" + i
     val mvLabels = testSubset.select(cols.head, cols.takeRight(cols.length - 1): _*).collect() // Arrays with predictions for every clf
       .map(row => row.toSeq // array of predictions
@@ -29,7 +29,7 @@ object FullTester {
     }
   }
 
-  def testWMv(testSubset: DataFrame, nClassif: Int, rects: Array[Array[Cube]], weightingFunction: Array[Cube] => Double): (Double, Double) = {
+  def testWMv(testSubset: DataFrame, nClassif: Int, rects: Array[Array[Cube]], weightingFunction: Array[Cube] => Double): Measurements = {
     val cols = for (i <- 0.until(nClassif)) yield PREDICTION + "_" + i
     val mvLabels = testSubset.select(FEATURES, cols: _*).collect() // Arrays with features values and predictions for every clf [features: SparseVector, prediction_0, prediction_1, ..., prediction_n]
       .map(row => row.toSeq.tail.indices.map(index => (row.get(index + 1).asInstanceOf[Double].doubleValue(), getWeightFromRects(rects, index, row.get(0), weightingFunction))) // array of predictions - weights tuples
@@ -41,7 +41,7 @@ object FullTester {
     calculateStatistics(mvLabels, refLabels)
   }
 
-  def testI(predictions: Array[Double], testSubset: DataFrame): (Double, Double) = {
+  def testI(predictions: Array[Double], testSubset: DataFrame): Measurements = {
     val refLabels = getReferenceLabels(testSubset)
     calculateStatistics(predictions, refLabels)
   }
@@ -53,14 +53,45 @@ object FullTester {
     }
   }
 
-  private def calculateStatistics(predLabels: Array[Double], refLabels: Array[Double]): (Double, Double) = {
-    val matched = predLabels.indices.map(i => (predLabels(i), refLabels(i))).groupBy(identity).mapValues(_.size)
-    val (tp, tn, fp, fn) = (matched.getOrElse((1, 1), 0), matched.getOrElse((0, 0), 0), matched.getOrElse((1, 0), 0), matched.getOrElse((0, 1), 0))
-    ((tp + tn).toDouble / (tp + tn + fp + fn),
-      (tp * tn - fp * fn).toDouble / math.sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn)))
+  private def calculateStatistics(predLabels: Array[Double], refLabels: Array[Double]): Measurements = {
+    val allIndexesSize = refLabels.length.toDouble
+    val allLabels = refLabels.distinct
+    val indexMap = allLabels.map(
+      label => (
+        refLabels.zipWithIndex.collect { case (l, i) if l == label => i }.toSet,
+        predLabels.zipWithIndex.collect { case (l, i) if l == label => i }.toSet
+      )
+    )
+    val acc = indexMap
+      .map { case (ref, pred) => (allIndexesSize - ((ref ++ pred).size - (ref & pred).size)) / allIndexesSize }
+      .sum / allLabels.length
+    val precissionMi = indexMap
+      .map { case (ref, pred) => (ref & pred).size }
+      .sum.toDouble / indexMap
+      .map { case (_, pred) => pred.size }
+      .sum
+    val recallMi = indexMap
+      .map { case (ref, pred) => (ref & pred).size }
+      .sum.toDouble / indexMap
+      .map { case (ref, _) => ref.size }
+      .sum
+    val precissionM = indexMap
+      .map { case (ref, pred) => (ref & pred).size.toDouble / pred.size }
+      .filter(!_.isNaN)
+      .sum / allLabels.length
+    val recallM = indexMap
+      .map { case (ref, pred) => (ref & pred).size.toDouble / ref.size }
+      .filter(!_.isNaN)
+      .sum / allLabels.length
+    Measurements(acc,
+      precissionMi, recallMi, fScore(precissionMi, recallMi, 1), // TODO: hardcoded beta
+      precissionM, recallM, fScore(precissionM, recallM, 1)
+    )
   }
 
-  def testRF(trainingSubset: DataFrame, testSubset: DataFrame, nClassif: Int): (Double, Double) = {
+  def fScore(precission: Double, recall: Double, beta: Double): Double = (math.pow(beta, 2) + 1) * precission * recall / (math.pow(beta, 2) * precission + recall)
+
+  def testRF(trainingSubset: DataFrame, testSubset: DataFrame, nClassif: Int): Measurements = {
     trainingSubset.cache()
     val predictions = new RandomForestClassifier()
       .setFeatureSubsetStrategy("auto")

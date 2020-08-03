9314b2842be0a6a0a12ca9826adcea13c1321264
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2020-08-03 21:43:42 +0200

    Revert "Fix model"
    This reverts commit 246e31b7

--- src/main/scala/jb/model/Measurements.scala
+++ src/main/scala/jb/model/Measurements.scala
@@ -1,21 +1,9 @@
 package jb.model
 
 case class Measurements(
-                         acc: Double,
-                         precissionMi: Double,
-                         recallMi: Double,
-                         fScoreMi: Double,
-                         precissionM: Double,
-                         recallM: Double,
-                         fScoreM: Double
-                       ) {
-  def toArray: Array[Double] = Array(
-    acc: Double,
-    precissionMi: Double,
-    recallMi: Double,
-    fScoreMi: Double,
-    precissionM: Double,
-    recallM: Double,
-    fScoreM: Double
-  )
-}
+                         measurementType: MeasurementType,
+                         mv: Double,
+                         rf: Double,
+                         wMvs: Array[Double],
+                         i: Array[Double]
+                       )
